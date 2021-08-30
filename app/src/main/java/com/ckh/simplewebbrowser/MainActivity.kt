945d3fb95f462c03c9e2e106965312c23bd9dc1a
ChoiKi <rlgns923@naver.com>
2021-08-30 12:57:54 +0900

    webview

--- app/src/main/java/com/ckh/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/ckh/simplewebbrowser/MainActivity.kt
@@ -4,18 +4,26 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
+import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private val addressBar : EditText by lazy {
-        findViewById(R.id.addressBar)
-    }
+    private val addressBar: EditText by lazy { findViewById(R.id.addressBar) }
+    private val refreshLayout: SwipeRefreshLayout by lazy { findViewById(R.id.refreshLayout) }
+    private val goHomeBtn: ImageButton by lazy { findViewById(R.id.homeBtn) }
+    private val goBackBtn: ImageButton by lazy { findViewById(R.id.backBtn) }
+    private val goFowardBtn: ImageButton by lazy { findViewById(R.id.forwartBtn) }
+    private val webView: WebView by lazy { findViewById(R.id.webView) }
+    private val progressBar: ContentLoadingProgressBar by lazy { findViewById(R.id.progressBar) }
 
-    private val webView:WebView by lazy {
-        findViewById(R.id.webView)
+    companion object {
+        private const val DEFAULT_URL = "https://www.google.com"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,20 +33,58 @@ class MainActivity : AppCompatActivity() {
         initView()
         bindView()
     }
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun initView(){
-        webView.apply{
-            webViewClient = WebViewClient()
-            settings.javaScriptEnabled = true
-            loadUrl("https://www.google.com")
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
-    private fun bindView(){
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initView() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl(DEFAULT_URL)
+        }
+    }
+
+    private fun bindView() {
         addressBar.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE){
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
                 webView.loadUrl(v.text.toString())
+                val loadingUrl = v.text.toString()
+                if(URLUtil.isNetworkUrl(loadingUrl)){
+                    webView.loadUrl(loadingUrl)
+                }else{
+                    webView.loadUrl("http://$loadingUrl")
+                }
             }
             return@setOnEditorActionListener false
+        }
+        goBackBtn.setOnClickListener {
+            webView.goBack()
+        }
+        goFowardBtn.setOnClickListener {
+            webView.goForward()
+        }
+        goHomeBtn.setOnClickListener {
+            webView.loadUrl(DEFAULT_URL)
+        }
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+    inner class WebViewClient:android.webkit.WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            refreshLayout.isRefreshing=false
+            progressBar.hide()
+            goBackBtn.isEnabled = webView.canGoBack()
+            goFowardBtn.isEnabled = webView.canGoBack()
+            addressBar.setText(url)
         }
     }
 }
